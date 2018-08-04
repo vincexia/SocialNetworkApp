7dbadfebc3cb98e0d70f1ee482f913c3da56c583
vincexia <vincexia@users.noreply.github.com>
2018-08-04 19:01:34 +0800

    update the main method with exception handle

--- social_network_app.py
+++ social_network_app.py
@@ -12,6 +12,7 @@ import sys
 import argparse
 from data_file_reader import load_data
 from network_data_manager import NetworkDataManager
+from search_operation import search_distance
 
 
 def main():
@@ -20,11 +21,30 @@ def main():
 
     # load data into data_manager
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    load_data(dir_path + '/SocialNetwork.txt', data_manager)
+    load_data(dir_path + '/SocialNetwork_5.txt', data_manager)
 
+    name_A = "MARTIN_OMERSA"
+    name_B = "CHRIS_POLAND"
 
+    try:
+        id_A = data_manager.person_dict[name_A]
+        id_B = data_manager.person_dict[name_B]
 
+        record_A = data_manager.record_dict[id_A]
+        record_B = data_manager.record_dict[id_B]
 
+        final_result = search_distance(id_A, record_A, id_B, record_B, data_manager)
+
+        if final_result < 0:
+            print ("No minimum distance found for {} and {}".format(name_A, name_B))
+        else:
+            print ("Minimum distance for {} and {} is {}".format(name_A, name_B, final_result))
+
+    except KeyError as e:
+        print ("KeyError {}. Please check the correctness of input name".format(e))
+    except:
+        print ("Unexpected error:", sys.exc_info()[0])
+        raise
 
 if __name__ == '__main__':
     main()
